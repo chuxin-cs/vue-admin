782f51f0a36e40082e30fcf3f11608139881caff
chuxin <15115359055@139.com>
2023-05-12 17:36:49 +0800

    feat: add styles

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,17 @@ import store from "./store";
 import {loadPlugins} from '@/plugins';
 
 // css
+import "normalize.css";
 import 'element-plus/dist/index.css';
+import "./styles/index.scss";
 
-const app = createApp(App)
+const app = createApp(App);
 
 /** 加载插件 **/
-loadPlugins(app)
+loadPlugins(app);
 /** 加载自定义指令 **/
 
 
 app.use(store).use(router);
-app.mount('#app')
+app.mount('#app');
 
